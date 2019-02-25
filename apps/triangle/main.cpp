b5ee5fd87fc260890b677cc595bb025b3649b643
Adrian Radziszewski <adra@itu.dk>
2019-02-25 12:49:41 +0100

    Models are not in the middle anymore (FUCK THIS UGLY MONSTROSITY)

--- apps/triangle/main.cpp
+++ apps/triangle/main.cpp
@@ -1394,13 +1394,14 @@ public:
 	}
 
 	void updateUniformBuffer2(Mesh* mesh) {
+		static float offset = 1.0f;
 		static auto startTime = std::chrono::high_resolution_clock::now();
-
+		offset = -offset;
 		auto currentTime = std::chrono::high_resolution_clock::now();
 		float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
 
 		UniformBufferObject ubo = {};
-		ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f)) * glm::translate(glm::mat4(1.0f), glm::vec3(offset, 0.0f, 0.0f));
 		ubo.view = glm::lookAt(glm::vec3(0.0f, 4.0f, -6.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 		ubo.proj = glm::perspective(glm::radians(45.0f), swapChainExtent.width / (float)swapChainExtent.height, 0.1f, 10.0f);
 		ubo.proj[1][1] *= -1;
