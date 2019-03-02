7628098d30157ea6f31f72d27fdd3d3b6c59d37a
Adrian Radziszewski <adra@itu.dk>
2019-03-02 02:36:08 +0100

    Made pipeline creation function signatures

--- apps/triangle/Pipeline.hpp
+++ apps/triangle/Pipeline.hpp
@@ -9,5 +9,18 @@ namespace Renderer::Pipeline
         VkPipelineLayout layout;
     };
 
+    VkPipelineShaderStageCreateInfo GetVertexShaderStageInfo();
+    VkPipelineShaderStageCreateInfo GetFragmentShaderStageInfo();
+    VkPipelineVertexInputStateCreateInfo GetVertexInputInfo();
+    VkPipelineInputAssemblyStateCreateInfo GetInputAssemblyCreateInfo();
+    VkPipelineDepthStencilStateCreateInfo GetDepthStencilCreateInfo();
+    VkPipelineViewportStateCreateInfo GetViewPortCreateInfo();
+    VkPipelineRasterizationStateCreateInfo GetRasterizerCreateInfo();
+    VkPipelineMultisampleStateCreateInfo GetMultisamplingCreateInfo();
+    VkPipelineColorBlendAttachmentState GetColorBlendAttachment();
+    VkPipelineColorBlendStateCreateInfo GetColorBlendCreateInfo();
+    VkPipelineLayoutCreateInfo GetPipelineCreateInfo();
+    VkPipelineLayout CreatePipelineLayout(VkDescriptorSetLayout layout);
+    VkGraphicsPipelineCreateInfo CreatePipelineCreateInfo(); //Worth it? Would have too many parameters.
     
 }
