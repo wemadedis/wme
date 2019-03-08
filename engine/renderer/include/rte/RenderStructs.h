29ddbc35ef31a7a05839cd0f62c469b5b35b4875
Adrian Radziszewski <adra@itu.dk>
2019-03-08 11:44:05 +0100

    Started work on image manager

---
+++ engine/renderer/include/rte/RenderStructs.h
@@ -0,0 +1,61 @@
+// TODO: (danh 22/02 09:57): Place this in appropriate Renderer-library when it exists
+#pragma once
+#include <glm\glm.hpp>
+namespace RTE_Renderer
+{
+	/// <summary>Vertex struct for use in RTE rendering.</summary>
+	struct Vertex
+	{
+		glm::vec3 Position;
+		glm::vec3 Normal;
+		glm::vec4 Color;
+	};
+
+	struct Camera
+	{
+		glm::vec3 Position;
+		glm::mat4 ViewMatrix;
+		glm::mat4 ProjectionMatrix;
+	};
+
+	enum class LightType {DIRECTIONAL, POINT, AMBIENT};
+
+	struct Light
+	{
+		LightType LightType;
+		glm::vec4 Color;
+		union
+		{
+			struct
+			{
+				glm::vec3 Direction;
+			};
+			struct
+			{
+				float Radius;
+				float Intensity;
+			};
+		};
+	};
+
+	struct Texture
+	{
+
+	};
+
+	struct Shader
+	{
+	};
+
+	struct Material
+	{
+		Texture* _Texture;
+	};
+
+	struct Mesh
+	{
+		int _VertexCount;
+		Vertex* _Vertices;
+		Material _Material;
+	};
+};
