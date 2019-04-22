fd29c373349be14903df351cbc7367bc474ff389
Adrian Radziszewski <adra@itu.dk>
2019-04-22 12:52:55 +0200

    Update on glm wrapper

--- engine/utilities/include/rte/GlmWrapper.hpp
+++ engine/utilities/include/rte/GlmWrapper.hpp
@@ -1,3 +1,10 @@
 #pragma once
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/euler_angles.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtx/rotate_vector.hpp>
