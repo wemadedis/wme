7bbc6b928d1e43954a7eb4d2e83caa8420c0ad28
Adrian Radziszewski <adra@itu.dk>
2019-05-03 00:56:55 +0200

    Added documentation for mesh and both light components

--- engine/StdComponents/MeshComponent.cpp
+++ engine/StdComponents/MeshComponent.cpp
@@ -5,10 +5,6 @@
 namespace RTE::StdComponents
 {
 
-MeshComponent::MeshComponent()
-{
-}
-
 void MeshComponent::Initialize(TransformComponent *transform, std::string meshFilePath, std::string textureFilePath)
 {
     _renderingManager = RenderingManager::GetInstance();
