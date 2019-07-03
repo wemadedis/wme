54ae61ee4afa560a0094fd28aa0b90f5997d8916
Freemedude <daniel_g_hansen@hotmail.com>
2019-07-03 14:36:14 +0200

    Renamed project #1

--- pseudocode/making_a_mesh.cpp
+++
@@ -1,9 +0,0 @@
-void Initialize()
-{
-    GameObject player = Scene.GetGameObject();
-    auto mc = player.AddComponent<MeshComponent>();
-
-    MeshHandle dragonHandle =  AssetManager::ImportModel("Path/To/My/File.ply");
-
-    mc.Init(dragonHandle);
-}
